b23bf46a2ce0d747d31d045ceb1d06aec5c6d9e0
Michel <michelmokbel@hotmail.com>
2025-01-30 12:21:19 +0300

    modified:   ios/Runner/Info.plist 	modified:   lib/assets/html/privacy_policy_en.html 	modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,41 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:moviemagicbox/screens/welcome_screen.dart';
 import 'package:moviemagicbox/services/movie_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:appsflyer_sdk/appsflyer_sdk.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 
 Future<void> preloadCache() async {
   final prefs = await SharedPreferences.getInstance();
-
   if (!prefs.containsKey("preloaded")) {
     await MovieService.fetchAllByType("movie");
     await MovieService.fetchAllByType("tv_show");
-
     prefs.setBool("preloaded", true);
   }
 }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  RequestConfiguration configuration = RequestConfiguration(
-    testDeviceIds: ['00008030-0004481C1185402E'],
-  );
-  MobileAds.instance.updateRequestConfiguration(configuration);
+
+  // Configure test device for ads
+  // RequestConfiguration configuration = RequestConfiguration(
+  //   testDeviceIds: ['00008030-0004481C1185402E'],
+  // );
+  // MobileAds.instance.updateRequestConfiguration(configuration);
+
   await Firebase.initializeApp();
-  // Fetch dev_key from Firebase Remote Config
-  String devKey = await fetchDevKeyFromRemoteConfig();
-  initAppsFlyer(devKey);
+
+  // Request App Tracking Transparency permission
+  TrackingStatus status = await AppTrackingTransparency.requestTrackingAuthorization();
+  bool isTrackingAllowed = status == TrackingStatus.authorized;
+
+  if (isTrackingAllowed) {
+    // Fetch AppsFlyer dev_key from Firebase Remote Config
+    String devKey = await fetchDevKeyFromRemoteConfig();
+    initAppsFlyer(devKey, isTrackingAllowed);
+  }
 
   await preloadCache();
   runApp(const MyApp());
@@ -37,66 +46,55 @@ void main() async {
 
 Future<String> fetchDevKeyFromRemoteConfig() async {
   final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
-
   try {
-    // Set default values (optional)
     await remoteConfig.setDefaults(<String, dynamic>{
-      'dev_key': 'TVuiYiPd4Bu5wzUuZwTymX', // Default value if the key isn't available
+      'dev_key': 'TVuiYiPd4Bu5wzUuZwTymX', // Default value if Remote Config fails
     });
-
-    // Fetch and activate the remote config values
     await remoteConfig.fetchAndActivate();
-
-    // Get the dev_key value
     String devKey = remoteConfig.getString('dev_key');
     print('Fetched dev_key: $devKey');
-
     return devKey;
   } catch (e) {
     print('Error fetching dev_key from Remote Config: $e');
-    return 'TVuiYiPd4Bu5wzUuZwTymX'; // Fallback to default value in case of an error
+    return 'TVuiYiPd4Bu5wzUuZwTymX'; // Fallback dev key
   }
 }
 
-void initAppsFlyer(String devKey) {
-  // SDK Options
+void initAppsFlyer(String devKey, bool isTrackingAllowed) {
+  // Set timeToWaitForATTUserAuthorization based on tracking permission
+  final double timeToWait = isTrackingAllowed ? 10 : 0;
+
   final AppsFlyerOptions options = AppsFlyerOptions(
-      // afDevKey: "TVuiYiPd4Bu5wzUuZwTymX",
       afDevKey: devKey,
       appId: "6741157554",
       showDebug: true,
-      timeToWaitForATTUserAuthorization: 15,
+      timeToWaitForATTUserAuthorization: timeToWait, // Set based on permission
       manualStart: false);
 
   final appsflyerSdk = AppsflyerSdk(options);
 
-  // Initialization of the AppsFlyer SDK
-  appsflyerSdk.initSdk(
-      registerConversionDataCallback: true,
-      registerOnAppOpenAttributionCallback: true,
-      registerOnDeepLinkingCallback: true);
-
-  // Starting the SDK with optional success and error callbacks
-  appsflyerSdk.startSDK(
-    onSuccess: () {
-      print("AppsFlyer SDK initialized successfully.");
-    },
-    onError: (int errorCode, String errorMessage) {
-      print(
-          "Error initializing AppsFlyer SDK: Code $errorCode - $errorMessage");
-    },
-  );
+  if (isTrackingAllowed) {
+    // Initialize AppsFlyer SDK ONLY if tracking is allowed
+    appsflyerSdk.initSdk(
+        registerConversionDataCallback: true,
+        registerOnAppOpenAttributionCallback: true,
+        registerOnDeepLinkingCallback: true);
+    appsflyerSdk.startSDK(
+      onSuccess: () => print("AppsFlyer SDK initialized successfully."),
+      onError: (int errorCode, String errorMessage) =>
+          print("Error initializing AppsFlyer SDK: Code $errorCode - $errorMessage"),
+    );
+  } else {
+    print("Tracking denied, skipping AppsFlyer initialization.");
+  }
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
-
   @override
   State<MyApp> createState() => _MyAppState();
 
-  // Static method to change the locale from anywhere in the app.
   static void setLocale(BuildContext context, Locale newLocale) {
-    // Find the nearest _MyAppState in the widget tree
     final _MyAppState? state = context.findAncestorStateOfType<_MyAppState>();
     state?.setLocale(newLocale);
   }
